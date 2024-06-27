60d23c185d2ee213abc483d87cd3a0d9b15b2fec
randyuncle <randylee609@email.com>
2024-06-27 16:59:49 +0800

    client: Apply test with continuous nodes
    This version of the client introduces testing with continuous nodes and
    a file output function to save the results of the kernel sorting test
    for specific sorting programs. Additionally, the implementation of the
    single node number test has been modified to apply different sorting
    programs, similar to the continuous node test.
    
    For the continuous node test, it is designed to test the sorting
    programs with different numbers of nodes. For each node count, the
    sorting programs will be tested with 10 loops.
    
    However, with the current implementation of the kernel device module and
    user space client program, triggering the test causes severe lag on the
    device, leading to crashes. The exact segment of code in the kernel
    device module causing this issue is unclear. Further testing and more
    accurate logging techniques are needed to identify the source of the
    problem.

--- client.c
+++ client.c
@@ -5,10 +5,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "sort.h"
 
 #define SORT_DEV "/dev/sort_test"
+
+#define LISTSORT "ls_data"
+#define TIMMERGE "tm_data"
+#define TIMLINEAR "tl_data"
+#define TIMBINARY "tb_data"
+#define TIMLGALLOP "tlg_data"
+#define TIMBGALLOP "tbg_data"
+#define ADAPSHIVER "ads_data"
+#define ADSMERGE "adsm_data"
+
+unsigned long long int duration[LOOP];
+size_t count[LOOP];
+
+typedef struct {
+    char *name;
+} directory;
+
+directory dirs[] = {
+    {.name = LISTSORT}, {.name = TIMMERGE}, {.name = TIMLINEAR}, 
+    {.name = TIMBINARY}, {.name = TIMLGALLOP}, {.name = TIMBGALLOP}, 
+    {.name = ADAPSHIVER}, {.name = ADSMERGE}, {.name = NULL}
+};
+
+static void file_output(size_t num, int case_id, char *dir_name)
+{
+    char cnt_file[100];
+    char time_file[100];
+
+    switch (case_id) {
+    case 0: /* Worst case of merge sort */
+        sprintf(cnt_file, "%s/w_count.txt", dir_name);
+        sprintf(time_file, "%s/w_time.txt", dir_name);
+        break;
+    case 1: /* Random 3 elements */
+        sprintf(cnt_file, "%s/r3_count.txt", dir_name);
+        sprintf(time_file, "%s/r3_time.txt", dir_name);
+        break;
+    case 2: /* Random last 10 elements */
+        sprintf(cnt_file, "%s/rl10_count.txt", dir_name);
+        sprintf(time_file, "%s/rl10_time.txt", dir_name);
+        break;
+    case 3: /* Random 1% elements */
+        sprintf(cnt_file, "%s/r1p_count.txt", dir_name);
+        sprintf(time_file, "%s/r1p_time.txt", dir_name);
+        break;
+    case 4: /* Duplicate */
+        sprintf(cnt_file, "%s/dup_count.txt", dir_name);
+        sprintf(time_file, "%s/dup_time.txt", dir_name);
+        break;
+    default: /* Random elements */
+        sprintf(cnt_file, "%s/r_count.txt", dir_name);
+        sprintf(time_file, "%s/r_time.txt", dir_name);
+        break;
+    }
+
+    FILE *cnt = fopen(cnt_file, "a");
+    FILE *time = fopen(time_file, "a");
+
+    if (!cnt) {
+        perror("The output file `(.)_count.txt` might have been collapsed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (!time) {
+        perror("The output file `(.)_time.txt` might have been collapsed");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0 ; i < LOOP ; i++) {
+        fprintf(cnt, "%lu %lu\n", num, count[i]);
+        fprintf(time, "%lu %llu\n", num, duration[i]);
+    }
+
+    fclose(cnt);
+    fclose(time);
+}
 
 static void sort_test_one_num(size_t num)
 {
@@ -16,57 +93,98 @@ static void sort_test_one_num(size_t num)
         perror("Given number argument out of the range of number of nodes in this code");
         exit(EXIT_FAILURE);
     }
-
+    
     int fd = open(SORT_DEV, O_RDWR);
     if (fd < 0) {
         perror("Failed to open character device");
         exit(EXIT_FAILURE);
     }
-
-    FILE *cnt = fopen("data/data_count.txt", "w");
-    FILE *time = fopen("data/data_time.txt", "w");
-
-    /* Testing mode. In reality, it should have 6 cases for testing */
-    for (int case_id = 0 ; case_id < 1 ; case_id++) {
-        st_dev st;
-        st.nodes = num;
-        st.case_id = case_id;
-
-        /* write the properties of the current test case to device driver */
-        if (write(fd, &st, sizeof(st)) < 0) {
-            perror("Failed to write to the device");
-            close(fd);
-            exit(EXIT_FAILURE);
-        }
-
-        st_usr out_st;
-        /* read the output of the test from device driver */
-        if (read(fd, &out_st, sizeof(out_st)) < 0) {
-            perror("Failed to read from the device");
-            close(fd);
-            exit(EXIT_FAILURE);
-        }
-
-        for (int i = 0 ; i < LOOP ; i++) {
-            fprintf(cnt, "%d %lu\n", i + 1, out_st.count[i]);
-            fprintf(time, "%d %llu\n", i + 1, out_st.time[i]);
-        }
-    }
-
-    close(fd);
-    fclose(time);
-    fclose(cnt);
     
+    int d = 0;
+    for (int case_id = 0 ; case_id < 6 ; case_id++) {
+        for (directory *dir = dirs ; dir->name ; d++, dir++) {
+            for (int i = 0 ; i < LOOP ; i++) {
+                st_dev st;
+                st.nodes = num;
+                st.case_id = case_id;
+                st.test_case = d;
+                /* write the properties of the current test case to device driver */
+                if (write(fd, &st, sizeof(st)) < 0) {
+                    perror("Failed to write to the device");
+                    close(fd);
+                    exit(EXIT_FAILURE);
+                }
+
+                printf("start to read\n");
+                st_usr out_st;
+                /* read the output of the test from device driver */
+                if (read(fd, &out_st, sizeof(out_st)) < 0) {
+                    perror("Failed to read from the device");
+                    close(fd);
+                    exit(EXIT_FAILURE);
+                }
+
+                count[i] = out_st.count;
+                duration[i] = out_st.time;
+            }
+
+            file_output(num, case_id, dir->name);
+        }
+
+        d = 0;
+    }
+    close(fd);
 }
 
-// static void sort_test_progress(size_t num)
-// {
-    
-// }
+static void sort_test_continuesly()
+{
+    int d = 0;
+    for (int case_id = 0 ; case_id < 6 ; case_id++) {
+        for (directory *dir = dirs ; dir->name ; d++, dir++) {
+            for (int num = MIN_LEN ; num < MAX_LEN - 20 ; num++) {
+                printf("start %s in #%d\n", dir->name, num);
+                for (int i = 0 ; i < LOOP ; i++) {
+                    int fd = open(SORT_DEV, O_RDWR);
+                    if (fd < 0) {
+                        perror("Failed to open character device");
+                        exit(EXIT_FAILURE);
+                    }
+
+                    st_dev st;
+                    st.nodes = num;
+                    st.case_id = case_id;
+                    st.test_case = d;
+                    /* write the properties of the current test case to device driver */
+                    if (write(fd, &st, sizeof(st)) < 0) {
+                        perror("Failed to write to the device");
+                        close(fd);
+                        exit(EXIT_FAILURE);
+                    }
+
+                    st_usr out_st;
+                    /* read the output of the test from device driver */
+                    if (read(fd, &out_st, sizeof(out_st)) < 0) {
+                        perror("Failed to read from the device");
+                        close(fd);
+                        exit(EXIT_FAILURE);
+                    }
+
+                    count[i] = out_st.count;
+                    duration[i] = out_st.time;
+
+                    close(fd);
+                }
+                file_output(num, case_id, dir->name);
+                sleep(1);
+            }
+        }
+        d = 0;
+    }
+}
 
 int main()
 {
-    sort_test_one_num(20000);
-
+    //sort_test_one_num(100);
+    sort_test_continuesly();
     return 0;
 }
